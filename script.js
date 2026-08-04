6dcb709c100635c00fe290428eeb9d8b3e5734d8
Bilal5166 <158145859+Bilal5166@users.noreply.github.com>
2026-08-04 16:18:57 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// User guess game
-//let guessnumber=Math.floor(Math.random()*10)+1;
-let guesnumber=7;
-let user=0;
-while(user!==guesnumber){
-    user=Number(prompt("enter number to guess"));
-    if(user<guesnumber){
-        alert("big number")
-
-    }
-    else if(user>guesnumber){
-        alert("small number")
-    }
-    else{
-        alert("Perfect match")
-    }
-
-}
 
 //Print table of any Number
 function Userinput(){
